80097e5cb4cd7466a29158a8146f2241438740ee
Drycca-paixao <dryccapaixao@hotmail.com>
2023-03-24 15:54:29 -0300

    classe usuario

--- src/main/java/br/com/cursovaldir/apicursovaldir/ApiCursoValdirApplication.java
+++ src/main/java/br/com/cursovaldir/apicursovaldir/ApiCursoValdirApplication.java
@@ -1,5 +1,6 @@
 package br.com.cursovaldir.apicursovaldir;
 
+import br.com.cursovaldir.apicursovaldir.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class ApiCursoValdirApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiCursoValdirApplication.class, args);
+        SpringApplication.run(ApiCursoValdirApplication.class, args);
     }
 
+    User user = new User(1, "drycca","mail@mail.com",  "123");
 }
---
+++ src/main/java/br/com/cursovaldir/apicursovaldir/domain/User.java
@@ -0,0 +1,27 @@
+package br.com.cursovaldir.apicursovaldir.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
