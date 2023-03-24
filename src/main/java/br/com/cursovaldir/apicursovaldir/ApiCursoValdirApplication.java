18752523318ae75e4ab40ea40b5fa9c1dde21a87
Drycca-paixao <dryccapaixao@hotmail.com>
2023-03-24 14:38:44 -0300

    iniciando projeto

---
+++ src/main/java/br/com/cursovaldir/apicursovaldir/ApiCursoValdirApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursovaldir.apicursovaldir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCursoValdirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCursoValdirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/cursovaldir/apicursovaldir/ApiCursoValdirApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursovaldir.apicursovaldir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursoValdirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
